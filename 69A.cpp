fe5885eca117340395ea5251dd27de37707f5634
Gaurav232003 <gauravagarwal232003>
2023-03-13 09:15:39 +0530

    69A

---
+++ 69A.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    int a=0,b=0,c=0;
+    while(t--){
+        int x,y,z;cin>>x>>y>>z;
+        a+=x;b+=y;c+=z;
+    }
+    if(a==0 && b==0 && c==0) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
